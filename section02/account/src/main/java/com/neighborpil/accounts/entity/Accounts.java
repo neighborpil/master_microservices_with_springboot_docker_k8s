2d524c8d98d9795fb5567e0f7c8bff0aeb96f98c
Neighborpil <neighborpil@naver.com>
2025-12-25 21:12:20 +1300

    feat:aditing

--- section02/account/src/main/java/com/neighborpil/accounts/entity/Accounts.java
+++ section02/account/src/main/java/com/neighborpil/accounts/entity/Accounts.java
@@ -2,8 +2,10 @@ package com.neighborpil.accounts.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
@@ -11,6 +13,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Accounts extends BaseEntity {
 
     @Column(name = "customer_id")
